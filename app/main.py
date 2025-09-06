3e60adf1d68c524882db18517244398f7abb8a65
Mykhaylo Zhyhan <mz5157e@gre.ac.uk>
2025-09-06 21:05:31 +0100

    created Herbivore and Carnivore child classes of class Animal and realized all tasks requested

--- app/main.py
+++ app/main.py
@@ -1,1 +1,35 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str = "",
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, animal: Animal) -> None:
+        if not isinstance(animal, Herbivore):
+            return
+        if animal.hidden or animal.health <= 0:
+            return
+
+        animal.health -= 50
+
+        if animal.health <= 0:
+            animal.health = 0  # clamp to zero
+            if animal in Animal.alive:
+                Animal.alive.remove(animal)
